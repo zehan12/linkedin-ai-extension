dd8e9d7ca0dce4e4fe6a66ea76ba1b19085d24b6
zehan12 <zehan9211@gmail.com>
2024-06-28 14:50:00 +0530

    chore: remove plasmo folder from git

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,9 +1,6 @@
-import App from "~App"
-
 function IndexPopup() {
   return (
     <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <App />
     </div>
   )
 }
